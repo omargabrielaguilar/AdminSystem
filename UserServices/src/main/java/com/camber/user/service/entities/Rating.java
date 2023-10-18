be319bbbc3cae8b06b2f0e01a2d5f5a7650e2473
Omar <saucyfunnel@gmail.com>
2023-10-18 18:14:46 -0500

    microservicio user rating finalizado

---
+++ UserServices/src/main/java/com/camber/user/service/entities/Rating.java
@@ -0,0 +1,25 @@
+package com.camber.user.service.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "ratings")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Rating {
+    @Id
+    @Column(name = "ID")
+    private String ratingId;
+    private String userId;
+    private String hotelId;
+    private int rating;
+    private String feedback;
+
+}
--- UserServices/src/main/java/com/camber/user/service/entities/UserEntity.java
+++ UserServices/src/main/java/com/camber/user/service/entities/UserEntity.java
@@ -1,10 +1,10 @@
 package com.camber.user.service.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -25,4 +25,8 @@ public class UserEntity {
     private String email;
     @Column(name = "ABOUT")
     private String about;
+
+    //Rating
+    @Transient
+    private List<Rating> ratings = new ArrayList<>();
 }
