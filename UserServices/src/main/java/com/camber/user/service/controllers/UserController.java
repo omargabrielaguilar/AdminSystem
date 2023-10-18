0c2cc818113052beb381b56711a2917b805dfad0
Omar <saucyfunnel@gmail.com>
2023-10-18 15:26:19 -0500

    microservicio user controller

--- UserServices/src/main/java/com/camber/user/service/controllers/UserController.java
+++ UserServices/src/main/java/com/camber/user/service/controllers/UserController.java
@@ -5,9 +5,9 @@ import com.camber.user.service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -16,8 +16,24 @@ public class UserController {
     private UserService userService;
 
     //create
+    @PostMapping
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user){
         UserEntity userEntity = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userEntity);
     }
+
+    // Search user for id
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserEntity> getSinglesUser(@PathVariable String userId){
+        UserEntity userEntity = userService.getUser(userId);
+        return ResponseEntity.ok(userEntity);
+    }
+
+    // listar
+    @GetMapping
+    public ResponseEntity<List<UserEntity>> getAllUser(){
+        List<UserEntity> allUser = userService.getAllUser();
+        return ResponseEntity.ok(allUser);
+    }
+
 }
