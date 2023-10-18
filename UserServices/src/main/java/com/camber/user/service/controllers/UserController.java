45cadf46cb11f113ed17dd23ae3222e674197e92
Omar <saucyfunnel@gmail.com>
2023-10-18 15:07:46 -0500

    microservicio pt1 65%

--- UserServices/src/main/java/com/camber/user/service/controllers/UserController.java
+++ UserServices/src/main/java/com/camber/user/service/controllers/UserController.java
@@ -1,4 +1,23 @@
 package com.camber.user.service.controllers;
 
+import com.camber.user.service.entities.UserEntity;
+import com.camber.user.service.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    //create
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user){
+        UserEntity userEntity = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userEntity);
+    }
 }
