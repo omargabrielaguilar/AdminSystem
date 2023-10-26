ca2b0d4fae508eaa417b5a3324524f1bdd93c6c0
Omar <saucyfunnel@gmail.com>
2023-10-25 22:42:57 -0500

    fin hotel microservices

--- HotelService/src/main/java/com/camber/hotel/controllers/HotelController.java
+++ HotelService/src/main/java/com/camber/hotel/controllers/HotelController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "/hotels")
+@RequestMapping("/hotels")
 public class HotelController {
     @Autowired
     private HotelService hotelService;
