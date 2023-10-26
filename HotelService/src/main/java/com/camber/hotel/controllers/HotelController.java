98b1a6030d9543430e6843de28acdd0a6acb634f
Omar <saucyfunnel@gmail.com>
2023-10-25 21:28:11 -0500

    fin controller y service impl

--- HotelService/src/main/java/com/camber/hotel/controllers/HotelController.java
+++ HotelService/src/main/java/com/camber/hotel/controllers/HotelController.java
@@ -1,10 +1,14 @@
 package com.camber.hotel.controllers;
 
+import com.camber.hotel.entities.Hotel;
 import com.camber.hotel.repositories.HotelRepository;
 import com.camber.hotel.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(name = "/hotels")
@@ -13,6 +17,21 @@ public class HotelController {
     private HotelService hotelService;
 
     //create
-    //getAll
+    @PostMapping
+    public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
+        return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
+    }
+
     //getById
+    @GetMapping("/{hotelId}")
+    public ResponseEntity<Hotel> getHotel(@PathVariable String hotelId){
+        return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.get(hotelId));
+    }
+
+
+    //getAll
+    @GetMapping
+    public ResponseEntity<List<Hotel>> getAll(){
+        return ResponseEntity.ok(hotelService.getAll());
+    }
 }
--- HotelService/src/main/java/com/camber/hotel/services/impl/HotelServiceImpl.java
+++ HotelService/src/main/java/com/camber/hotel/services/impl/HotelServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class HotelServiceImpl implements HotelService {
@@ -16,6 +17,9 @@ public class HotelServiceImpl implements HotelService {
     private HotelRepository hotelRepository;
     @Override
     public Hotel create(Hotel hotel) {
+        String hotelId = UUID.randomUUID().toString();
+        hotel.setId(hotelId);
+
         return hotelRepository.save(hotel);
     }
 
