3f88a7360af7536aa55dda12c7923c60698b30ae
Omar <saucyfunnel@gmail.com>
2023-11-08 10:15:01 -0500

    trabajo

--- UserServices/src/main/java/com/camber/user/service/UserServicesApplication.java
+++ UserServices/src/main/java/com/camber/user/service/UserServicesApplication.java
@@ -3,7 +3,9 @@ package com.camber.user.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServicesApplication {
 
     public static void main(String[] args) {
