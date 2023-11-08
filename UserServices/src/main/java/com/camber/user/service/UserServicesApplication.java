adec9dfa6c2bbaefb9552e9ec7275ca840edf684
Omar <saucyfunnel@gmail.com>
2023-11-08 15:59:23 -0500

    eureka error

--- UserServices/src/main/java/com/camber/user/service/UserServicesApplication.java
+++ UserServices/src/main/java/com/camber/user/service/UserServicesApplication.java
@@ -2,12 +2,12 @@ package com.camber.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
 public class UserServicesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserServicesApplication.class, args);
     }
