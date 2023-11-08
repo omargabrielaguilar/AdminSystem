e9c882992575d1c48b27bad4118cc8e9d3f0ad3f
Omar <saucyfunnel@gmail.com>
2023-11-08 17:04:32 -0500

    solución eureka client

--- UserServices/src/main/java/com/camber/user/service/UserServicesApplication.java
+++ UserServices/src/main/java/com/camber/user/service/UserServicesApplication.java
@@ -2,11 +2,8 @@ package com.camber.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 
 @SpringBootApplication
-@EnableEurekaClient
 public class UserServicesApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServicesApplication.class, args);
