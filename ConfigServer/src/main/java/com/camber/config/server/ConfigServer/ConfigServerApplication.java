fc9788c2ae62f49424e6c8a3629d3d1f5ab4ce81
Omar <saucyfunnel@gmail.com>
2023-11-15 10:18:36 -0500

    config profiles

--- ConfigServer/src/main/java/com/camber/config/server/ConfigServer/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/camber/config/server/ConfigServer/ConfigServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServerc
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
