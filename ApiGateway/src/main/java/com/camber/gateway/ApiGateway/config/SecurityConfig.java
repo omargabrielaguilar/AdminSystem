23bddd5f0962bd6bb984225b8bd38719674865c4
Omar <saucyfunnel@gmail.com>
2023-11-19 22:48:02 -0500

    conf

---
+++ ApiGateway/src/main/java/com/camber/gateway/ApiGateway/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.camber.gateway.ApiGateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+}
